d5076cb03e877b05ab5cf248ee8d89d6fdea19a6
simpsonphile <simpsonphile@gmail.com>
2020-05-17 15:44:11 +0200

    Make board images show current board imgs

--- components/TheBoardImages.vue
+++ components/TheBoardImages.vue
@@ -1,19 +1,29 @@
 <template>
-    <div class="c-board-list">
-        <img
-            v-for="img in getImages('mlp')"
+    <div class="c-board-images">
+        <div
+            v-for="img in getImages(currentBoard)"
             :key="img.url"
-            :src="img.url"
-            :alt="img.fileName" />
+            class="c-board-images__img">
+            <img
+                :src="img.url"
+                :alt="img.fileName"
+                class="o-image o-image--fit" />
+        </div>
+
+        <button @click="$store.dispatch('fetchImagesFromBoard', currentBoard)">update {{currentBoard}}</button>
     </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import { mapFields } from 'vuex-map-fields'
 
 export default {
     name: 'the-board-images',
     computed: {
+        ...mapFields({
+            currentBoard: 'currentBoard'
+        }),
         ...mapGetters({
             getImages: 'getBoardImages'
         })
@@ -22,14 +32,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .c-board-list {
+    .c-board-images {
         flex-wrap: wrap;
 
         display: flex;
+    }
 
-        & > * {
-            margin: 5px;
-            width: 200px;
-        }
+    .c-board-images__img {
+        margin: 5px;
+        width: 200px;
+        height: 200px;
     }
 </style>
